28b53cbabd8f06f4642f160ed7f5f2fb0a35a9f8
Rafael Dohms <rdohms@gmail.com>
2013-11-23 23:01:19 +0100

    Blog Posts
    - added blog integration
    - tweaked vagrant and ansible

---
+++ src/AmsterdamPHP/Bundle/SiteBundle/Service/BlogService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AmsterdamPHP\Bundle\SiteBundle\Service;
+
+use Guzzle\Http\Client;
+use Predis\Client as Cache;
+
+class BlogService
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $url = 'http://www.amsterdamphp.nl/';
+
+    /**
+     * @var Cache
+     */
+    protected $cache;
+
+    /**
+     * Constructor
+     */
+    public function __construct(Cache $cache)
+    {
+        $this->client = new Client($this->url);
+        $this->cache = $cache;
+    }
+
+    /**
+     * Returns the latest blog posts
+     * 
+     * @return mixed
+     */
+    public function getLatestBlogPosts()
+    {
+        $cacheKey = 'blog.rss.latest';
+        $postsRss = $this->cache->get($cacheKey);
+
+        if ($postsRss === null){
+            $postsRss = base64_encode(serialize($this->client->get('/feed')->send()));
+
+            $this->cache->set($cacheKey, $postsRss);
+            $this->cache->expireat($cacheKey, strtotime('+5 hours'));
+        }
+
+        $postsRss = unserialize(base64_decode($postsRss));
+
+        return (new \SimpleXMLElement($postsRss->getBody()))->channel->item;
+    }
+}
